c96637f9efaaae73f8149f516236a7a57b39c5a1
PoiseAdeSetiawan <poisea.setiawan@gmail.com>
2025-05-19 10:01:52 +0700

    adding contact, footer, and whatsapp-buble

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+//hamburger menu
+document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const navigation = document.querySelector('.navigation');
-    hamburger.addEventListener('click', function() {
+    hamburger.addEventListener('click', function () {
         navigation.classList.toggle('open');
     });
 });
+
+//whatsapp buble
+function redirectToWhatsApp() {
+    window.location.href = 'https://wa.me/62895640966000';
+}
